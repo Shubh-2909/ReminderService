a18a4d5ea7479b263ec416fb5d675db4d6a4b90a
Shubh Shubhanjal <shubhshubhanjal96@gmail.com>
2024-02-06 18:46:46 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const app = express();
 const bodyParser = require('body-parser')
 const {PORT} = require('./config/serverConfig')
-const cron = require('node-cron');
+// const cron = require('node-cron');
+const jobs = require('./utils/job')
+const TicketController = require('./controllers/ticket-controller')
 // const {sendBasicEmail} = require('./services/email-services')
 const setupAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
+    app.post('/api/v1/tickets' , TicketController.create);
 
     app.listen(PORT , () => {
-      console.log(`Server started on port ${PORT}`)
+      console.log(`Server started on port ${PORT}`);
+      jobs();
 
     //   sendBasicEmail(
     //     'shubhshubhanjal96@gmail.com',
@@ -18,9 +23,9 @@ const setupAndStartServer = () => {
     //     'Hey , how are you?'
     //   )
 
-    cron.schedule('* * * * *', () => {
-        console.log('running a task every minute');
-      });
+    // cron.schedule('* * * * *', () => {
+    //     console.log('running a task every minute');
+    //   });
     });
 }
 
--- src/services/email-services.js
+++ src/services/email-services.js
@@ -1,4 +1,6 @@
 const sender = require('../config/emailConfig')
+const TicketRepository = require('../repository/ticket-repository')
+
 
 const sendBasicEmail = async (mailFrom , mailTo , mailSubject , mailBody) => {
     try {
@@ -14,8 +16,41 @@ const sendBasicEmail = async (mailFrom , mailTo , mailSubject , mailBody) => {
     }
 }
 
+const fetchPendingEmails = async(timestamp) => {
+    try {
+        const repo = new TicketRepository();
+        const response = await repo.get({status: "PENDING"});
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const createNotification = async(data) => {
+    try {
+        const repo = new TicketRepository();
+        const response = await repo.create(data);
+        return response;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const updateTicket = async (ticketId , data) => {
+    try {
+        const repo = new TicketRepository();
+        const response = await repo.update(ticketId , data);
+        return response;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 module.exports={
-    sendBasicEmail
+    sendBasicEmail,
+    fetchPendingEmails,
+    createNotification,
+    updateTicket
 }
 
 

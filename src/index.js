45a7f217c30b29bab24fae9c4a59b35ae461005d
Shubh Shubhanjal <shubhshubhanjal96@gmail.com>
2024-02-06 14:09:53 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const {PORT} = require('./config/serverConfig')
-const {sendBasicEmail} = require('./services/email-services')
+const cron = require('node-cron');
+// const {sendBasicEmail} = require('./services/email-services')
 const setupAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
@@ -10,12 +11,16 @@ const setupAndStartServer = () => {
     app.listen(PORT , () => {
       console.log(`Server started on port ${PORT}`)
 
-      sendBasicEmail(
-        'shubhshubhanjal96@gmail.com',
-        'shubhshubhanjal7@gmail.com',
-        'This is a testing email',
-        'Hey , how are you?'
-      )
+    //   sendBasicEmail(
+    //     'shubhshubhanjal96@gmail.com',
+    //     'shubhshubhanjal7@gmail.com',
+    //     'This is a testing email',
+    //     'Hey , how are you?'
+    //   )
+
+    cron.schedule('* * * * *', () => {
+        console.log('running a task every minute');
+      });
     });
 }
 
